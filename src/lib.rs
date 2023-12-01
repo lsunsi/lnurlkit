457338bb5b89e1e89c07f5272fa2b1ca2d7efd44
Lucas Sunsi Abreu <lsunsi@pm.me>
2023-12-01 09:01:58 -0300

    Attempt at LUD-01

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,40 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+pub struct Lnurl(url::Url);
+
+impl TryFrom<&str> for Lnurl {
+    type Error = &'static str;
+
+    fn try_from(s: &str) -> Result<Self, Self::Error> {
+        let Ok((hrp, data, _)) = bech32::decode(s) else {
+            return Err("bech32 decode failed");
+        };
+
+        if hrp != "lnurl" {
+            return Err("bech32 hrp invalid");
+        }
+
+        let Ok(bytes) = <Vec<u8> as bech32::FromBase32>::from_base32(&data) else {
+            return Err("bech32 data is not bytes");
+        };
+
+        let Ok(text) = String::from_utf8(bytes) else {
+            return Err("bech32 bytes is not string");
+        };
+
+        let Ok(url) = url::Url::parse(&text) else {
+            return Err("bech32 text is not a url");
+        };
+
+        Ok(Lnurl(url))
+    }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
-
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn try_from() {
+        let input = "LNURL1DP68GURN8GHJ7UM9WFMXJCM99E3K7MF0V9CXJ0M385EKVCENXC6R2C35XVUKXEFCV5MKVV34X5EKZD3EV56NYD3HXQURZEPEXEJXXEPNXSCRVWFNV9NXZCN9XQ6XYEFHVGCXXCMYXYMNSERXFQ5FNS";
+        let lnurl: super::Lnurl = input.try_into().expect("parse");
+
+        assert_eq!(lnurl.0.to_string(), "https://service.com/api?q=3fc3645b439ce8e7f2553a69e5267081d96dcd340693afabe04be7b0ccd178df");
     }
 }
